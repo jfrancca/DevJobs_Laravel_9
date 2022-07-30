b951cd31cd3d2d9f5f8551ecd3d24c704e3edb96
juan camilo <juancamilofrancocardona@gmail.com>
2022-07-30 09:51:43 -0500

    editar-vacante

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,6 @@
 use App\Http\Controllers\VacanteController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -20,5 +10,6 @@ Route::get('/', function () {
 
 Route::get('/dashboard', [VacanteController::class, 'index'])->middleware(['auth', 'verified'])->name('vacantes.index');
 Route::get('/vacantes/create', [VacanteController::class, 'create'])->middleware(['auth', 'verified'])->name('vacantes.create');
+Route::get('/vacantes/{vacante}/edit', [VacanteController::class, 'edit'])->middleware(['auth', 'verified'])->name('vacantes.edit');
 
 require __DIR__.'/auth.php';
